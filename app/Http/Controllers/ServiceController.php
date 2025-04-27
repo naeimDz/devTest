ef5d9b2e18bfadefad5f02e62b3133361de7d536
naeimDz <naeimdz1703@gmail.com>
2025-04-27 17:16:48 +0100

    review condition query services

--- app/Http/Controllers/ServiceController.php
+++ app/Http/Controllers/ServiceController.php
@@ -26,7 +26,7 @@ class ServiceController extends Controller
         $user = Auth::user();
         $query = Service::query();
         
-        if ($user->role === 'service_provider') {
+        if ($user->role->name !== 'admin') {
             $query->where('user_id', $user->id);
         }
         
